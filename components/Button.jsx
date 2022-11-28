b6f9eae38c02668540abd6e232a40086b5aeb250
Mario U. Yaoyao II <marioyaoyao14@gmail.com>
2022-11-28 21:23:57 +0800

    Add files via upload
    need services page and fix width for the landing page

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,17 @@
 import styles from "./Button.module.css";
 
-export default function Button() {
+export default function Button({ children, variant}) {
     return (
         <div>
-                
+            {variant === "primary" ? (
+                <div>
+                    <button className={styles.primary}>{children}</button>
+                </div>
+            ) : (
+                <div>
+                    <button className={styles.secondary}>{children}</button>
+                </div>
+            )}    
         </div>
     )
 }
